2274590df10ef4f580f855ef0320942cd654947e
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-11-03 14:45:02 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,97 @@
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],]
+
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(board, index, current_player = "X")
+    board[index] = current_player
+  end
+
+  def position_taken?(board, index)
+    !(board[index].nil? || board[index] == " ")
+  end
+
+  def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+  end
+
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+
+  # Define your play method below
+
+  def turn_count(board)
+    turns = board.detect do |i|
+      i == "X" || i == "O"
+    end
+    turn.count
+  end
+
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    winning_position = []
+    win_combination.each { |position| winning_position << board[position]}
+    if winning_position == ["X", "X", "X"] || winning_position == ["O", "O", "O"]
+      return win_combination
+    end
+  end
+  return false
+end
+
+def full?(board)
+ board.all?{|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+  if won?(board) || !full?(board)
+    return false
+  else
+    return true
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+return  board[won?(board)[0]]
+  end
+end
