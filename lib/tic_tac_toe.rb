b006a69e0326ec3968ec55a744ae7eb45b309efc
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-11-03 15:27:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ WIN_COMBINATIONS = [
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(board, index)
-      move(board, index)
+      move(board, index, current_player(board))
       display_board(board)
     else
       turn(board)
