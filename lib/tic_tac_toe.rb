e1c23958e971c67a332dbae23e42d7b08bd5da77
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-11-03 15:23:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,7 +25,7 @@ WIN_COMBINATIONS = [
     user_input.to_i - 1
   end
 
-  def move(board, index, current_player = "X")
+  def move(board, index, current_player(board))
     board[index] = current_player
   end
 
